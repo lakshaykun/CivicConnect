0716a5065fd3375884eb3c3b948b74f4e9f136e8
lakshaykun <lakshu1000@gmail.com>
2025-10-11 16:09:38 +0530

    initial

--- app/index.tsx
+++ app/index.tsx
@@ -1,25 +1,12 @@
-import { Stack, Link } from 'expo-router';
+import { View, Text } from 'react-native'
+import React from 'react'
 
-import { View } from 'react-native';
-
-import { Button } from '@/components/Button';
-import { Container } from '@/components/Container';
-import { ScreenContent } from '@/components/ScreenContent';
-
-export default function Home() {
+const index = () => {
   return (
-    <View className={styles.container}>
-      <Stack.Screen options={{ title: 'Home' }} />
-      <Container>
-        <ScreenContent path="app/index.tsx" title="Home"></ScreenContent>
-        <Link href={{ pathname: '/details', params: { name: 'Dan' } }} asChild>
-          <Button title="Show Details" />
-        </Link>
-      </Container>
+    <View>
+      <Text>index</Text>
     </View>
-  );
+  )
 }
 
-const styles = {
-  container: 'flex flex-1 bg-white',
-};
+export default index
